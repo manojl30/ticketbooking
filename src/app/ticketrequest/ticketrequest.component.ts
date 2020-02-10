7efb6c205249cf6637044b3aea5fcf0b08ac8dbe
Manojmanu1997 <46087707+Manojmanu1997@users.noreply.github.com>
2020-02-10 12:06:08 +0530

    Update ticketrequest.component.ts

--- src/app/ticketrequest/ticketrequest.component.ts
+++ src/app/ticketrequest/ticketrequest.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import * as ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-ticketrequest',
@@ -8,13 +9,14 @@ import * as ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 })
 export class TicketrequestComponent implements OnInit {
   public Editor = ClassicEditor;
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit() {
   }
+
 OnClick(problem,modules,priority){
-  localStorage.setItem("problem",JSON.stringify(problem.value));
-  localStorage.setItem("problem",JSON.stringify(modules.value));
-  localStorage.setItem("problem",JSON.stringify(priority.value));
-}
+  localStorage.setItem("problem",JSON.stringify(problem));
+  localStorage.setItem("modules",JSON.stringify(modules));
+  localStorage.setItem("priority",JSON.stringify(priority));
+  this.router.navigateByUrl('listrequest');}
 }
